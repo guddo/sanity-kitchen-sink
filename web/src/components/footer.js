668ae07e01c512d0486bc43393a5f0de980e3db1
Martin Cording <mc@inlead.dk>
2020-07-15 03:08:09 +0200

    Minor changes to components.

--- web/src/components/footer.js
+++ web/src/components/footer.js
@@ -21,10 +21,6 @@ const Footer = ({ siteTitle }) => (
             Rentemestervej 56C
             2400 København NV
           </p>
-            </li>
-            <li className="mt-2 inline-block mr-2 md:block md:mr-0">
-            </li>
-          </ul>
         </div>
         <div className="flex-1">
           <p className="uppercase text-gray-500 md:mb-6">
